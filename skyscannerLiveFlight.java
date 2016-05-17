725e49e313d457477f66c919cdeaf13b15a70d9d
Anthony Zeng <anthonyzeng99@gmail.com>
2016-05-17 01:06:30 -0400

    working api request

--- skyscannerLiveFlight.java
+++ skyscannerLiveFlight.java
@@ -1,25 +1,34 @@
-import java.sql.Date;
+import java.io.BufferedReader;
+import java.io.DataOutputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import javax.net.ssl.HttpsURLConnection;
+import java.util.Map;
+import java.util.LinkedHashMap;
+import java.net.URLEncoder;
 
 public class skyscannerLiveFlight {
 
-    private final String apiKey;
-    private final String country;
-    private final String currency;
-    private final String locale;
-    private final String originplace;
-    private final String destinationplace;
-    private final Date outbounddate;
-    private final Date inbounddate;
-    private final String locationschema;
-    private final String cabinclass;
-    private final int adults;
-    private final int children;
-    private final int infants;
-    private final boolean  groupPricing;
+    private String apiKey;
+    private String country;
+    private String currency;
+    private String locale;
+    private String originplace;
+    private String destinationplace;
+    private String outbounddate;
+    private String inbounddate;
+    private String cabinclass;
+    private int adults;
+    private int children;
+    private int infants;
+    private boolean  groupPricing;
 
+    private static final String USER_AGENT = "Mozilla/5.0";
     private String url = "https://partners.api.skyscanner.net/apiservices/pricing/v1.0";
+    private String sessionUrl;
 
-    public skyscannerLiveFlight(String apiKey, String country, String currency, String locale, String originplace, String destinationplace, Date outbounddate, Date inbounddate, String cabinclass, int adults, int children, int infants, boolean groupPricing) {
+    public skyscannerLiveFlight(String apiKey, String country, String currency, String locale, String originplace, String destinationplace, String outbounddate, String inbounddate, String cabinclass, int adults, int children, int infants, boolean groupPricing) {
 	this.apiKey = apiKey;
 	this.country = country;
 	this.currency = currency;
@@ -33,12 +42,89 @@ public class skyscannerLiveFlight {
 	this.children = children;
 	this.infants = infants;
 	this.groupPricing = groupPricing;
+	try {
+	    createSession();
+	} catch (Exception e) {
+
+	}
     }
 
-    public skyscannerLiveFlight(String apiKey, String originplace, String destinationplace, Date outbounddate, Date inbounddate, String cabinclass, int adults, int children, int infants) {
-	this(apiKey, "USA", "USD", "en-US", originplace, destinationplace, outbounddate, inbounddate, cabinclass, adults, children, infants, true);
+    public skyscannerLiveFlight(String apiKey, String originplace, String destinationplace, String outbounddate, String inbounddate, String cabinclass, int adults, int children, int infants) {
+	this(apiKey, "US", "USD", "en-US", originplace, destinationplace, outbounddate, inbounddate, cabinclass, adults, children, infants, true);
     }
 
+    private boolean createSession() throws Exception {
+
+        String url = "https://partners.api.skyscanner.net/apiservices/pricing/v1.0";
+	URL obj = new URL(url);
+	HttpURLConnection con = (HttpURLConnection) obj.openConnection();
+	
+	con.setRequestMethod("POST");
+	con.setRequestProperty("User-Agent", USER_AGENT);
+	con.setRequestProperty("Accept-Language", "en-US.en;q=0.5");
+	con.setRequestProperty("Content-Type", "application/x-www-form-urlencoded");
+	con.setRequestProperty("Accept", "application/json");
+	
+	Map<String,Object> params = new LinkedHashMap<>();
+	params.put("apiKey", apiKey);
+	params.put("country", country);
+	params.put("currency",currency);
+	params.put("locale", locale);
+	params.put("originplace", originplace);
+	params.put("destinationplace", destinationplace);
+	params.put("outbounddate", outbounddate);
+	params.put("inbounddate", inbounddate);
+	//params.put("cabinclass", cabinclass);
+	//params.put("adults", adults);
+	//params.put("children", children);
+	//params.put("infants", infants);
+	//params.put("groupPricing", groupPricing);
+	
+	StringBuilder postData = new StringBuilder();
+	for (Map.Entry<String,Object> param : params.entrySet()) {
+	    if (postData.length() != 0) {
+		postData.append('&');
+	    } 
+	    postData.append(URLEncoder.encode(param.getKey(), "UTF-8"));
+	    postData.append("=");
+	    postData.append(URLEncoder.encode(String.valueOf(param.getValue()), "UTF-8"));
+	}
+	
+
+	byte[] postDataBytes = postData.toString().getBytes("UTF-8");
+		
+	con.setDoOutput(true);
+	DataOutputStream wr = new DataOutputStream(con.getOutputStream());
+		
+	wr.write(postDataBytes);
+	wr.flush();
+	wr.close();
+	
+	int responseCode = con.getResponseCode();
+	System.out.println("Response code :" + responseCode);
+	
+	BufferedReader in = new BufferedReader(new InputStreamReader(con.getInputStream()));
+	String inputLine;
+	StringBuffer response = new StringBuffer();
+	
+	while ((inputLine = in.readLine()) != null) {
+	    response.append(inputLine);
+	}
+	in.close();
+	
+	System.out.println(response.toString());
+	sessionUrl = con.getHeaderField("Location");
+
+	if (responseCode == 201) {
+	    return true;
+	}
+	return false;
+    }
+
+        
+    public static void main(String[] args) {
+	skyscannerLiveFlight f = new skyscannerLiveFlight("an318955594945738134979842887915", "JFK-sky", "LAX-sky", "2016-06-20", "", "Economy", 1, 0, 0);
+	
+    }
     
-
 }
--- skyscannerRequest.java
+++ skyscannerRequest.java
@@ -4,36 +4,60 @@ import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
 import javax.net.ssl.HttpsURLConnection;
+import java.util.Map;
+import java.util.LinkedHashMap;
+import java.net.URLEncoder;
+import java.sql.Date;
 
 public class skyscannerRequest {
 
     private static final String USER_AGENT = "Mozilla/5.0";
 
     public static void createSession() throws Exception {
-       
+     
 	String url = "http://partners.api.skyscanner.net/apiservices/pricing/v1.0";
 	URL obj = new URL(url);
 	HttpURLConnection con = (HttpURLConnection) obj.openConnection();
 	
 	con.setRequestMethod("POST");
-	//con.setRequestMethod("User-Agent", USER_AGENT);
-	//con.setRequestMethod("Accept-Language", "en-US.en;q=0.5");
+	con.setRequestProperty("User-Agent", USER_AGENT);
+	con.setRequestProperty("Accept-Language", "en-US.en;q=0.5");
+	con.setRequestProperty("Content-Type", "application/x-www-form-urlencoded");
+	con.setRequestProperty("Accept", "application/json");
 	
-	String urlParameters = "";
-	    //"apikey=an619533267721094791408626689612&country=USA&currency=USD&locale=en-US&originplace=JFK&destinationplace=LAX&outbounddate = 2016-06-02";
-	
+	Map<String,Object> params = new LinkedHashMap<>();
+	params.put("apiKey", "an318955594945738134979842887915");
+	params.put("country", "US");
+	params.put("currency","USD");
+	params.put("locale", "en-US");
+	params.put("originplace", "JFK-sky");
+	params.put("destinationplace", "LAX-sky");
+	params.put("outbounddate", "2016-06-02");
+	params.put("inbounddate", "");
+	params.put("adults", 1);
+
+	StringBuilder postData = new StringBuilder();
+	for (Map.Entry<String,Object> param : params.entrySet()) {
+	    if (postData.length() != 0) {
+		postData.append('&');
+	    } 
+	    postData.append(URLEncoder.encode(param.getKey(), "UTF-8"));
+	    postData.append("=");
+	    postData.append(URLEncoder.encode(String.valueOf(param.getValue()), "UTF-8"));
+	}
+
+	byte[] postDataBytes = postData.toString().getBytes("UTF-8");
+
+
 	con.setDoOutput(true);
 	
 	DataOutputStream wr = new DataOutputStream(con.getOutputStream());
        
-	wr.writeBytes(urlParameters);
+	wr.write(postDataBytes);
 	wr.flush();
 	wr.close();
 	
-
 	int responseCode = con.getResponseCode();
-	System.out.println("\nSending 'POST' request to URL: " + url);
-	System.out.println("Post parameters :" + urlParameters);
 	System.out.println("Response code :" + responseCode);
 
 	BufferedReader in = new BufferedReader(new InputStreamReader(con.getInputStream()));
@@ -46,10 +70,11 @@ public class skyscannerRequest {
 	in.close();
 
 	System.out.println(response.toString());
-	System.out.println("done");
-    }
+	    }
 
     public static void pollSession() throws Exception {
+	
+	
 
     }
 
