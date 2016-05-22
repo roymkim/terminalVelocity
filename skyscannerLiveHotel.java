96ad1586815b8aaa73a391b86559016b36beec46
Roy Kim <rkim8@stuy.edu>
2016-05-22 00:33:46 -0400

    Why am I getting 403 Error?

--- skyscannerLiveHotel.java
+++ skyscannerLiveHotel.java
@@ -42,7 +42,7 @@ public class skyscannerLiveHotel{
     }
     
     public skyscannerLiveHotel(String apiKey, String entityid, String checkindate, String checkoutdate, int guests, int rooms){
-	this(apiKey, "US", "USD", "en-US", entityid, checkindate, checkoutdate, guests, rooms);
+	this(apiKey, "UK", "EUR", "en-GB", entityid, checkindate, checkoutdate, guests, rooms);
     }
 
     private boolean createSession() throws Exception{
@@ -50,9 +50,13 @@ public class skyscannerLiveHotel{
 	site+="/"+market+"/"+currency+"/"+locale+"/"+entityid+"/"+checkindate+"/"+checkoutdate+"/"+guests+"/"+rooms+"?apiKey="+apiKey;
 	//System.out.println(site);
 	URL url = new URL(site);
+	System.out.println(url);
 	HttpURLConnection con = (HttpURLConnection) url.openConnection();
 	con.setRequestMethod("GET");
-	BufferedReader rd = new BufferedReader(new InputStreamReader(con.getInputStream()));
+	con.connect();
+	int code = con.getResponseCode();
+	System.out.println(code);
+BufferedReader rd = new BufferedReader(new InputStreamReader(con.getInputStream()));
 	String line;
 	while ((line = rd.readLine()) != null){
 	    result.append(line);
@@ -60,7 +64,7 @@ public class skyscannerLiveHotel{
 	rd.close();
 	con.connect();
 	System.out.println("test");
-	int code = con.getResponseCode();
+	//int code = con.getResponseCode();
 	
 	//con.setRequestProperty("User-Agent", USER_AGENT);    
 
@@ -79,6 +83,6 @@ public class skyscannerLiveHotel{
     
 
     public static void main(String[]args){
-	skyscannerLiveHotel h = new skyscannerLiveHotel("an318955594945738134979842887915", "27539733", "2016-05-18", "2016-05-21", 2, 1);
+	skyscannerLiveHotel h = new skyscannerLiveHotel("prtl6749387986743898559646983194", "27539733", "2015-05-23", "2015-05-25", 2, 1);
     }
 }
