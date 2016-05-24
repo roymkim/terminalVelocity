62cd19c32035eef4b54cb6a2ced8380ac67aaf69
Anthony Zeng <anthonyzeng99@gmail.com>
2016-05-23 22:37:12 -0400

    parses results and print out formitted results

--- skyscannerLiveHotel.java
+++ skyscannerLiveHotel.java
@@ -1,5 +1,6 @@
-import java.net.*;
+ import java.net.*;
 import java.io.*;
+import com.eclipsesource.json.*;
 
 public class skyscannerLiveHotel{
     private final String apiKey;
@@ -13,7 +14,7 @@ public class skyscannerLiveHotel{
     private final int rooms;
 
     private static final String USER_AGENT = "Mozilla/5.0";
-    private String site = "http://partners.api.skyscanner.net/apiservices/hotels/liveprices/v2";
+    private String url = "http://partners.api.skyscanner.net/apiservices/hotels/liveprices/v2";
 
     public skyscannerLiveHotel(String apiKey, String market, String currency, String locale, String entityid, String checkindate, String checkoutdate, int guests, int rooms){
 	this.apiKey = apiKey;
@@ -27,8 +28,10 @@ public class skyscannerLiveHotel{
 	this.rooms = rooms;
 	try {
 	    createSession();
-	    pollSession();
+	    String results = pollSession();
+	    parseResult(results);
 	} catch (Exception e){
+	    System.out.println(e);
 	}
     }
     
@@ -36,33 +39,89 @@ public class skyscannerLiveHotel{
 	this(apiKey, "UK", "EUR", "en-GB", entityid, checkindate, checkoutdate, guests, rooms);
     }
 
-    public void createSession() throws Exception{
-	site+="/"+market+"/"+currency+"/"+locale+"/"+entityid+"/"+checkindate+"/"+checkoutdate+"/"+guests+"/"+rooms+"?apiKey="+apiKey;
-	URL url = new URL(site);
-	System.out.println(url);
-	HttpURLConnection con = (HttpURLConnection) url.openConnection();
-	con.setRequestMethod("GET");
-	con.connect();
-	int code = con.getResponseCode();
-	System.out.println(code);
+    public String buildParameters() {
+	String params = "/"+market+"/"+currency+"/"+locale+"/"+entityid+"/"+checkindate+"/"+checkoutdate+"/"+guests+"/"+rooms+"?apiKey="+apiKey;
+	return params;
     }
 
-    public void pollSession() throws Exception{
-	StringBuilder result = new StringBuilder();
-	URL url = new URL(site);
-	HttpURLConnection con = (HttpURLConnection) url.openConnection();
+    public void createSession() throws Exception{
+	String params = buildParameters();
+	URL urlObj = new URL(url + params);
+
+	HttpURLConnection con = (HttpURLConnection) urlObj.openConnection();
 	con.setRequestMethod("GET");
+	con.connect();
+
+	int responseCode = con.getResponseCode();
+	if (responseCode == 200) {
+	    System.out.println("Results found:");
+	    System.out.println();
+	}
+    }
+
+    public String pollSession() throws Exception{
+
+	String params = buildParameters();
+	URL urlObj = new URL(url + params);
+
+	HttpURLConnection con = (HttpURLConnection) urlObj.openConnection();
+	con.setRequestMethod("GET");
+
+	StringBuilder result = new StringBuilder();
 	BufferedReader rd = new BufferedReader(new InputStreamReader(con.getInputStream()));
 	String line;
 	while ((line = rd.readLine()) != null){
 	    result.append(line);
 	}
 	rd.close();
-	System.out.println(result.toString());
+	
+	return result.toString();
+
     }
 
-    
+    private void parseResult(String result) {
+	String[] hotelNames = new String[10];
+	String[] hotelStars = new String[10];
+	int[] hotelPrices = new int[10];
+
+	JsonObject resultObj = Json.parse(result).asObject();
+
+	JsonArray hotels_prices = resultObj.get("hotels_prices").asArray();
+	JsonArray hotels = resultObj.get("hotels").asArray();
+      
+
+	int entry = 0;
+	for(JsonValue hotel: hotels) {
+	    //Hotel Name
+	    hotelNames[entry] = hotel.asObject().getString("name", "idk");
+
+	    //Hotel Stars
+	    int stars = hotel.asObject().getInt("star_rating", 1);
+	    String stringOfStars = "";
+	    for (int i = 0; i < stars; i++) {
+		stringOfStars += "*";
+	    }
+	    hotelStars[entry] = stringOfStars;
+	    
+	    //Hotel Price
+	    JsonObject hotel_price = hotels_prices.get(entry).asObject();
+	    JsonArray agent_prices = hotel_price.get("agent_prices").asArray();
+	    hotelPrices[entry] = agent_prices.get(0).asObject().getInt("price_total", 1);
+
+	    entry++;
+	}
+
+	for (int i = 0; i < hotelNames.length; i++) {
+	    String resultEntry = " ";
+	    resultEntry += hotelNames[i] + "  ";
+	    resultEntry += hotelStars[i] + "  ";
+	    resultEntry += "$" + hotelPrices[i];
+	    System.out.println(resultEntry);
+	}
+
+    }
+
     public static void main(String[]args){
-	skyscannerLiveHotel h = new skyscannerLiveHotel("prtl6749387986743898559646983194", "27539733", "2016-05-23", "2016-05-25", 2, 1);
+	skyscannerLiveHotel h = new skyscannerLiveHotel("prtl6749387986743898559646983194", "27544008", "2016-07-23", "2016-07-25", 2, 1);
     }
 }
