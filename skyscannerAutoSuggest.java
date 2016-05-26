d43fce4bc43b1885730e8e445faedd85c5557bec
Roy Kim <rkim8@stuy.edu>
2016-05-26 13:08:17 -0400

    Update

--- skyscannerAutoSuggest.java
+++ skyscannerAutoSuggest.java
@@ -9,7 +9,7 @@ public class skyscannerAutoSuggest{
     private String query;
 
     private static String USER_AGENT = "Mozilla/5.0";
-    private String site = "http://partners.api.skyscanner.net/apiservices/autosuggest/v1.0/";
+    private String site = "http://partners.api.skyscanner.net/apiservices/autosuggest/v1.0";
 	
     public skyscannerAutoSuggest(String market, String currency, String locale, String apiKey, String query){
 	this.market = market;
@@ -17,24 +17,37 @@ public class skyscannerAutoSuggest{
 	this.locale = locale;
 	this.apiKey = apiKey;
 	this.query = query;
+	try{
+	    createSession();
+	    pollSession();
+	} catch (Exception e){
+	    System.out.println(e);
+	}
     }
 
     public skyscannerAutoSuggest(String apiKey, String query){
-	this("UK", "EUR", "en-GB", apiKey, query);
+	this("GB", "GBP", "en-GB", apiKey, query);
+    }
+
+    public String buildParameters(){
+	String params = "/"+market+"/"+currency+"/"+locale+"?query="+query+"&apiKey="+apiKey;
+	return params;
     }
     
     public void createSession() throws Exception{
-	site+="/"+market+"/"+currency+"/"+locale+"/?query="+query+"&apiKey="+apiKey;
-	URL url = new URL(site);
-	System.out.println(url);
+	String params = buildParameters();
+	URL url = new URL(site+params);
+
 	HttpURLConnection con = (HttpURLConnection) url.openConnection();
 	con.setRequestMethod("GET");
 	con.connect();
-	int code = con.getResponseCode();
-	System.out.println(code);	
+
+	int responseCode = con.getResponseCode();
+	System.out.println(url);
+	System.out.println(responseCode);	
     }
 
-    public void pollSession() throws Exception{
+    public String pollSession() throws Exception{
 	StringBuilder result = new StringBuilder();
 	URL url = new URL(site);
 	HttpURLConnection con = (HttpURLConnection) url.openConnection();
@@ -45,10 +58,10 @@ public class skyscannerAutoSuggest{
 	    result.append(line);
 	}
 	rd.close();
-	System.out.println(result.toString());
+	return result.toString();
     }
 
     public static void main(String[]args){
-	skyscannerAutoSuggest a = new skyscannerAutoSuggest("prtl6749387986743898559646983194", "New York"); 
+	skyscannerAutoSuggest a = new skyscannerAutoSuggest("prtl6749387986743898559646983194", "ed"); 
     }
 }
